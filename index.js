11fd0099146341c589e9aa2dbf75d69641c10a77
tranmanhthang96nd <tranmanhthang@cryptopie-labo.com>
2023-03-14 10:54:34 +0700

    init

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ const page_url = 'https://www.facebook.com/groups/ryanvanhungnguoiban/posts';
   });
 
   // 1. Lấy cookies Facebook
-  const FACEBOOK_COOKIES = [
-    { name: 'presence', value: 'C%7B%22t3%22%3A%5B%7B%22i%22%3A%22u.100065676313102%22%7D%5D%2C%22utc3%22%3A1678762805607%2C%22v%22%3A1%7D', domain: '.facebook.com' },
-    { name: 'fr', value: '0tYuUlpxo5asbbe44.AWVJMJ_clkwQkgCZC5iz4NRzALA.BkD-AR.BG.AAA.0.0.BkD-AR.AWWIGIsLNqw', domain: '.facebook.com' },
-    { name: 'xs', value: '21%3AIeuCwRgN5UNqQw%3A2%3A1677466401%3A-1%3A6386%3A%3AAcVOpkwNA-S3EcudIpzli82PK4NMir9AvtuFf7_AzvI', domain: '.facebook.com' },
-    { name: 'wd', value: '1391x261', domain: '.facebook.com' },
-    { name: 'm_pixel_ratio', value: '2', domain: '.facebook.com' },
-    { name: 'm_page_voice', value: '100009025734152', domain: '.facebook.com' },
-    { name: 'datr', value: 'QBr8Y0HS3nkoT54LCXkjc8I2', domain: '.facebook.com' },
-    { name: 'dpr', value: '2', domain: '.facebook.com' },
-    { name: 'c_user', value: '100009025734152', domain: '.facebook.com' },
-    { name: 'sb', value: 'QBr8Y9vv2lmG6KrpG70LYt_D', domain: '.facebook.com' },
-  ];
+  const FACEBOOK_COOKIES = [];
 
   // 2. Tiếp tục sử dụng Chrome tab để truy cập Facebook
   const page = await browser.newPage();
